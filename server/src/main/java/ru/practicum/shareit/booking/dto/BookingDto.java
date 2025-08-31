a7a5a1e239a3a84f595749b26a437d0ad33203f2
Ivan Liubimov <lordolub@gmail.com>
2025-08-31 14:22:51 +0300

    Second commit || add-item-requests-and-gateway || Logic done, Postman tests passed

---
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -0,0 +1,25 @@
+package ru.practicum.shareit.booking.dto;
+
+import lombok.*;
+import ru.practicum.shareit.booking.model.Status;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import java.time.LocalDateTime;
+
+/**
+ * TODO Sprint add-bookings.
+ */
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookingDto {
+    private Long id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private ItemDto item;
+    private UserDto booker;
+    private Status status;
+}
