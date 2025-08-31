eda18e1b28f28959390c086d5490c93e082ace1b
Ivan Liubimov <lordolub@gmail.com>
2025-08-31 14:53:04 +0300

    Merge remote-tracking branch 'origin/add-item-requests-and-gateway' into add-item-requests-and-gateway || Checkstyle Fix

--- gateway/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ gateway/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -6,8 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 
-import java.util.Collection;
-
 /**
  * TODO Sprint add-item-requests.
  */
--- gateway/src/main/java/ru/practicum/shareit/request/ItemRequestDtoValidator.java
+++ gateway/src/main/java/ru/practicum/shareit/request/ItemRequestDtoValidator.java
@@ -8,7 +8,7 @@ import ru.practicum.shareit.request.dto.ItemRequestDto;
 @Component
 public class ItemRequestDtoValidator {
 
-    public void validate (ItemRequestDto itemRequestDto) {
+    public void validate(ItemRequestDto itemRequestDto) {
         if (itemRequestDto.getDescription() == null || itemRequestDto.getDescription().isBlank()) {
             throw new ConditionsNotMetException("Описание запроса не может быть пустым");
         }
