7d4a194943d281bd928cee8dfd7657a626d5ab73
Ivan Liubimov <lordolub@gmail.com>
2025-09-02 14:33:22 +0300

    Merge remote-tracking branch 'origin/add-item-requests-and-gateway' into add-item-requests-and-gateway || Fixes after first review || Checkstyle fixed

--- server/src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.item.service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.repository.BookingRepository;
