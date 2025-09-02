908932e408423ef6c50c9a3a4e17d9d90893ef4d
Ivan Liubimov <lordolub@gmail.com>
2025-09-02 14:33:46 +0300

    Merge remote-tracking branch 'origin/add-item-requests-and-gateway' into add-item-requests-and-gateway || Fixes after first review || Checkstyle fixed

--- server/src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -17,7 +17,7 @@ import ru.practicum.shareit.request.repository.ItemRequestRepository;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
 
-import java.awt.print.Pageable;
+
 import java.time.Instant;
 import java.util.*;
 import java.util.stream.Collectors;
